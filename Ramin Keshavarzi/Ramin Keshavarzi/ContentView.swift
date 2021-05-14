bc086b542f42f60fa6f26a05310bf24e2ba68f2c
Ramin Keshavarzi <raminkeshavarzi@organiplace.com>
2021-05-14 08:17:03 +0200

    Inserted elements such as title, photo in stack

--- Ramin Keshavarzi/Ramin Keshavarzi/ContentView.swift
+++ Ramin Keshavarzi/Ramin Keshavarzi/ContentView.swift
@@ -9,8 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color(.systemBlue).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack{
+                Image("Ramin").resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 150, height: 150, alignment: .center)
+                    .cornerRadius(10)
+                    .shadow(radius: 10)
+                    .padding()
+                
+                Text("Ramin Keshavarzi").fontWeight(.semibold)
+                    .font(.system(size: 35))
+                    .foregroundColor(.white)
+        
+                
+                Text("Entrepreneur").fontWeight(.medium)
+                    .font(.headline)
+                    .foregroundColor(.black)
+            }
+        }
+       
     }
 }
 
